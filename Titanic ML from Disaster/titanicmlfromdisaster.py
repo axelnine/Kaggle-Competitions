e84291e68d8e5c38b18fb6a5d3d387b196ba82d8
Prem Shah <premparag@gmail.com>
2018-01-04 23:19:50 -0800

    Added files

---
+++ Titanic ML from Disaster/titanicmlfromdisaster.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+#Since the
+#description of the competition mentions that women and
+# children have a more chance to survive, we will engineer the features accordingly.
